5340d0d6669484ace7cce9cb1edb35679c02dbc3
NoelleWright <118686588+NoelleWright@users.noreply.github.com>
2023-01-12 21:29:14 -0600

    Create Pypoll Challenge.py

---
+++ Pypoll Challenge.py
@@ -0,0 +1,31 @@
+
+Election Results
+-------------------------
+Total Votes: 369,711
+-------------------------
+
+County Votes:
+Jefferson: 0.0% (1)
+Denver: 0.0% (1)
+Arapahoe: 0.0% (1)
+
+-------------------------
+Largest County Turnout: Jefferson
+Winning County Vote: 1
+Winning County Percentage: 0.0%
+-------------------------
+
+Candidate Percentage of Votes:
+-------------------------
+
+Charles Casper Stockham: 0.0% (1)
+
+Diana DeGette: 0.0% (1)
+
+Raymon Anthony Doane: 0.0% (1)
+
+-------------------------
+Winner: 
+Winning Vote Count: 1
+Winning Percentage: 0.0%
+-------------------------
